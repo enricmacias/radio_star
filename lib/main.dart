9f1663763625b17896ca19428fc019820d868891
Enric Macias Lopez <enric.macias.lopez@gmail.com>
2025-06-08 16:06:12 +0200

    Create MiniPlayerView

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'views/home_view.dart';
+import 'views/home/home_view.dart';
 
 void main() {
-  runApp(
-    ProviderScope(
-      child: MaterialApp(
-      home: HomeView(),
-    ),
-    ),
-  );
+  runApp(ProviderScope(child: MaterialApp(home: HomeView())));
 }
---
+++ lib/views/home/home_view.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:radio_star/views/favorite_view.dart';
+import 'package:radio_star/views/search_view.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'mini_player_view.dart';
+
+part 'home_view.g.dart';
+
+@riverpod
+class PageIndex extends _$PageIndex {
+  @override
+  int build() => 0;
+
+  void onDestinationSelected(int index) {
+    state = index;
+  }
+}
+
+class HomeView extends ConsumerWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    int pageIndex = ref.watch(pageIndexProvider);
+
+    return Scaffold(
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          MiniPlayerView(),
+          NavigationBar(
+            onDestinationSelected: (int index) {
+              ref.read(pageIndexProvider.notifier).onDestinationSelected(index);
+            },
+            selectedIndex: pageIndex,
+            destinations: const <Widget>[
+              NavigationDestination(
+                selectedIcon: Icon(Icons.search),
+                icon: Icon(Icons.search_outlined),
+                label: 'Search',
+              ),
+              NavigationDestination(
+                selectedIcon: Icon(Icons.star),
+                icon: Icon(Icons.star_outline),
+                label: 'Favorite Stations',
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: <Widget>[SearchView(), FavoriteView()][pageIndex],
+    );
+  }
+}
